2d8626f978fa5d220cd4044c634ae5b17a87e1b6
LJKhnor <135867702+LJKhnor@users.noreply.github.com>
2024-10-28 11:52:22 +0100

    skeleton and start of home page

---
+++ src/components/icons/IconAge.vue
@@ -0,0 +1,27 @@
+<template>
+  <svg-icon id="mdi-cake" type="mdi" :path="path"></svg-icon>
+</template>
+
+<script lang="ts">
+import SvgIcon from '@jamescoyle/vue-icon'
+import { mdiCakeVariant } from '@mdi/js'
+
+export default {
+  name: 'my-cool-component',
+
+  components: {
+    SvgIcon,
+  },
+  setup(props, ctx) {
+    return {
+      path: mdiCakeVariant,
+    }
+  },
+}
+</script>
+<style>
+.mdi-cake {
+  width: 25px;
+  height: 25px;
+}
+</style>
---
+++ src/components/icons/IconMdi.vue
@@ -0,0 +1,24 @@
+<template>
+  <svg-icon id="mdi-{{mdiIconName}}" type="mdi" :path="mdiIconName"></svg-icon>
+</template>
+
+<script lang="ts">
+import SvgIcon from '@jamescoyle/vue-icon'
+
+export default {
+  name: 'my-cool-component',
+  components: {
+    SvgIcon,
+  },
+  props: ['mdiIconName'],
+  setup(props, ctx) {
+    return {}
+  },
+}
+</script>
+<style>
+.mdi-cake {
+  width: 25px;
+  height: 25px;
+}
+</style>
