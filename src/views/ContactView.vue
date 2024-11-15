4eda12f2f2abe14db79ac71d1047bf3853e741ae
LJKhnor <135867702+LJKhnor@users.noreply.github.com>
2024-11-15 14:44:56 +0100

    fin des corrections

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -4,15 +4,15 @@
       <ul>
         <li>
           <label for="name">Nom : </label>
-          <input type="text" id="name" name="user_name" />
+          <input type="text" id="user_name" name="user_name" />
         </li>
         <li>
           <label for="mail">E-mail : </label>
-          <input type="email" id="mail" name="user_mail" />
+          <input type="email" id="user_mail" name="user_mail" />
         </li>
         <li>
           <label for="msg">Message : </label>
-          <textarea id="msg" name="user_message"></textarea>
+          <textarea id="user_message" name="user_message"></textarea>
         </li>
       </ul>
       <div class="button">
